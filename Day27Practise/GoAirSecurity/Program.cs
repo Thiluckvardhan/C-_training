691d9198e8c946578440197d80ed1ea02ab05fd4
Thiluck Vardhan Vemula <youremail@example.com>
2026-01-30 09:44:02 +0530

    Day28 Unit Testing

---
+++ Day27Practise/GoAirSecurity/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace GoAirSecurity
+{
+    public class EntryUtility
+    {
+        public bool validateEmployeeId(string employeeId)
+        {
+            bool result=true;
+            if (employeeId == null)
+            {
+                throw new Exception(AbandonedMutexException);
+            }
+            if("GOAIR")
+        }
+    }
+}
