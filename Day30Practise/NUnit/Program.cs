5dc8df093bdb2b6076f499decd20ec640efa10aa
Thiluck Vardhan Vemula <youremail@example.com>
2026-02-02 16:31:32 +0530

    NUnit

--- Day30Practise/NUnit/Program.cs
+++ Day30Practise/NUnit/Program.cs
@@ -1,37 +1,42 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 public class BankAccount
 {
-    public decimal Balance { get; set; }
+    public decimal Balance { get; private set; }
 
     public BankAccount(decimal initialBalance)
     {
+        if (initialBalance < 0)
+            throw new ArgumentException("Initial balance cannot be negative");
+
         Balance = initialBalance;
     }
 
     public void Deposit(decimal amount)
     {
-        if (amount < 0)
-        {
-            throw new Exception("Deposit amount cannot be negative");
-        }
+        if (amount <= 0)
+            throw new ArgumentException("Deposit amount must be positive");
+
         Balance += amount;
     }
 
     public void Withdraw(decimal amount)
     {
+        if (amount <= 0)
+            throw new ArgumentException("Withdrawal amount must be positive");
+
         if (amount > Balance)
-        {
-            throw new Exception("Insufficient funds");
-        }
+            throw new InvalidOperationException("Insufficient funds");
+
         Balance -= amount;
     }
 }
 
 [TestFixture]
-public class UnitTest
+public class BankAccountTests
 {
-    private BankAccount? account;
+    private BankAccount account;
 
     [SetUp]
     public void Setup()
@@ -40,28 +45,34 @@ public class UnitTest
     }
 
     [Test]
-    public void Test_Deposit_ValidAmount()
+    public void Deposit_ValidAmount_IncreasesBalance()
     {
-        account!.Deposit(500);
+        account.Deposit(500);
         Assert.That(account.Balance, Is.EqualTo(1500));
     }
 
     [Test]
-    public void Test_Deposit_NegativeAmount()
+    public void Deposit_NegativeAmount_ThrowsException()
     {
-        Assert.Throws<Exception>(() => account!.Deposit(-100));
+        Assert.Throws<ArgumentException>(() => account.Deposit(-100));
     }
 
     [Test]
-    public void Test_Withdraw_ValidAmount()
+    public void Withdraw_ValidAmount_DecreasesBalance()
     {
-        account!.Withdraw(300);
+        account.Withdraw(300);
         Assert.That(account.Balance, Is.EqualTo(700));
     }
 
     [Test]
-    public void Test_Withdraw_InsufficientFunds()
+    public void Withdraw_InsufficientFunds_ThrowsException()
     {
-        Assert.Throws<Exception>(() => account!.Withdraw(1500));
+        Assert.Throws<InvalidOperationException>(() => account.Withdraw(1500));
+    }
+
+    [Test]
+    public void Withdraw_NegativeAmount_ThrowsException()
+    {
+        Assert.Throws<ArgumentException>(() => account.Withdraw(-200));
     }
 }
