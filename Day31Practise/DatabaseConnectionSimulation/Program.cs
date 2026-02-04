a952d6c41d014ca5317cc7766c8633d0da43f8d6
Thiluck Vardhan Vemula <youremail@example.com>
2026-02-04 13:42:39 +0530

    DataBase Connection

---
+++ Day31Practise/DatabaseConnectionSimulation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class DatabaseConnection
+{
+    static void Main()
+    {
+        // TODO:
+        // 1. Open connection
+        bool isConnected=false;
+        try
+        {
+            System.Console.WriteLine("Opening DataBase Connection.");
+            isConnected=true;
+
+            throw new Exception("DataBase Operation Failure");
+        }
+        // 2. Simulate operation failure
+        catch(Exception ex)
+        {
+            System.Console.WriteLine(ex.Message);
+        }
+        // 3. Ensure connection is closed properly
+        finally
+        {
+            if (isConnected)
+            {
+                isConnected=false;
+                System.Console.WriteLine("DataBase Connection Closed.");
+            }
+        }
+    }
+}
---
+++ Day31Practise/SafeNumericInputHandling/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class InputHandler
+{
+    static void Main()
+    {
+        // TODO:
+        // 1. Read input from user
+        // 2. Handle invalid numeric input
+        // 3. Keep asking until valid number is entered
+        while (true)
+        {
+            System.Console.Write("Enter only Numeric Input: ");
+            if(int.TryParse(Console.ReadLine(),out int input))
+            {
+                System.Console.WriteLine("Input taken Sucessfully");
+                break;
+            }
+        }
+    }
+}
