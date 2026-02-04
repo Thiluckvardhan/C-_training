53273dc0a65ea4a0e0dd526197cc6b15616ca393
Thiluck Vardhan Vemula <youremail@example.com>
2026-02-04 13:30:02 +0530

    exception handling

---
+++ Day31Practise/CustomException–LoginAttempts/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class LimitException : Exception
+{
+    public LimitException(string message): base(message)
+    {
+        
+    }
+}
+class LoginSystem
+{
+    static void Main()
+    {
+        int attempts = 0;
+        // TODO
+        // 1. Allow only 3 login attempts
+        string password="ABCDE";
+        string inputpassword=null;
+        try
+        {
+        while (true)
+        {
+            if (attempts >= 3)
+            {
+        // 2. Create and throw custom exception after limit
+                throw new LimitException("Password Attempt Limit reached");
+            }
+            System.Console.Write("Enter your Password: ");
+            inputpassword=Console.ReadLine();
+            attempts++;
+            if (inputpassword == null)
+            {
+                continue;
+            }
+            if (password == inputpassword)
+            {
+                System.Console.WriteLine("Correct Password");
+                break;
+            }
+        }
+        }
+        // 3. Handle exception and terminate application
+        catch (LimitException ex)
+        {
+            System.Console.WriteLine($"{ex.Message}");
+        }
+        catch(Exception ex)
+        {
+            System.Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ Day31Practise/ExceptionPropagationAcrossLayers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Controller
+{
+    static void Main()
+    {
+        // TODO:
+        // Call Service method
+        try
+        {
+        Service.Process();
+        }
+        catch(Exception ex)
+        {
+            System.Console.WriteLine(ex.Message);
+        }
+        // Handle exception here
+    }
+}
+
+class Service
+{
+    public static void Process()
+    {
+        // TODO:
+        // Call Repository method
+        try
+        {
+            Repository.GetData();
+        }
+        // Catch, log and rethrow exception
+        catch(Exception ex)
+        {
+            Console.WriteLine("Logging exception in Service layer");
+            throw;
+        }
+    }
+}
+
+class Repository
+{
+    public static void GetData()
+    {
+        // TODO:
+        // Throw an exception here
+        throw new Exception("Data Not Found");
+    }
+}
