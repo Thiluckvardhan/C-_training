82ba97094841d9de53dc3e7db86dd73ccd7b34bf
Thiluck Vardhan Vemula <youremail@example.com>
2026-02-04 11:32:29 +0530

    EmployeeBonusProcessing

---
+++ Day31Practise/EmployeeBonusProcessing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class BonusCalculator
+{
+    static void Main()
+    {
+        int[] salaries = { 5000, 0, 7000 };
+        int bonus = 10000;
+
+        for (int i = 0; i < salaries.Length; i++)
+        {
+            try
+            {
+                Console.WriteLine(bonus / salaries[i]);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero salary.");
+            }
+        }
+    }
+}
